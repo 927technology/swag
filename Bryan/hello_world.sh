bd32990619aa37fd9335792b37bd04087938a089
Bryan stewart <bryan.stewart.2015@gmail.com>
2025-02-27 20:16:06 -0600

    initial commit

--- Bryan/hello_world.sh
+++ Bryan/hello_world.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+### created by Bryan Stewart
+### bryan.stewart.2015@gmail.com
+### date created 20250227
 
+curl -sk https://raw.githubusercontent.com/927technology/swag/refs/heads/main/Bryan/dependencies.sh | bash
+echo "Hello World Mutha fuckka"
