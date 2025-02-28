e98652ff03dbdb614ca64257943138c017c57459
Chris Murray <83325346+927technology@users.noreply.github.com>
2025-02-28 11:37:57 -0600

    Update functions.sh

--- Bryan/functions.sh
+++ Bryan/functions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+. ./nopeeking.a
+
 # hello world function
 say_hello() {
   local name="$1"
   echo Hello, ${name}!
 }
-
-. ./nopeeking.a
 
 ### calling the function 
 say_hello "$1"
